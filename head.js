a551351831efebd8775d3afd1008633dbf99e1de
Andrew <andrew.ku93@gmail.com>
2021-02-01 21:58:29 +0000

    implement the head function

--- head.js
+++ head.js
@@ -10,3 +10,15 @@ const assertEqual = function(actual, expected) {
 // Test
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
+const head = function(array) {
+  if (array.length < 1) {
+    return undefined;
+  } else {
+    return array[0];
+  }
+};
+
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
