e2a0181d09dec4e09b8db9ff74c8ba4c4047c73e
Andrew <andrew.ku93@gmail.com>
2021-02-01 22:21:33 +0000

    add tail function implementation

---
+++ tail.js
@@ -0,0 +1,23 @@
+// Function Implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  if (array.length < 2) {
+    return [];
+  } else {
+    return array.slice(1);
+  }
+};
+
+// Test original
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+console.log(tail(words));
