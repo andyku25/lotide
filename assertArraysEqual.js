0bfc2c3967ab26b53991fddc3403682ac9cbfe5c
Andrew <andrew.ku93@gmail.com>
2021-02-03 00:06:37 +0000

    complete assertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,11 @@
 // Check to see if 2 arrays are equal
 const eqArrays = function(array1, array2) {
-  let match = true;
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      match = false;
-      return match;
+      return false;
     }
   }
-  return match;
+  return true;
 };
 
 const assertArraysEqual = function(actual, expected) {
@@ -21,4 +19,4 @@ const assertArraysEqual = function(actual, expected) {
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2",3]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
