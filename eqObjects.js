97943cdff5b07becf1e50d930983291276ec92e2
Andrew <andrew.ku93@gmail.com>
2021-02-03 23:43:11 +0000

    added eqArrays formula and passed step 1 checks

--- eqObjects.js
+++ eqObjects.js
@@ -6,6 +6,19 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
 // return true if both objects 1) have the same number of keys. 
 //                             2) have the same values for the same keys
 const eqObjects = function(object1, object2) {
@@ -14,13 +27,21 @@ const eqObjects = function(object1, object2) {
   // 1) Check the length of the keys
   if (object1Keys.length === object2Keys.length) {
     for (const key of object1Keys) {
-      // 2) Check the values of each key to see if they match
-      if (object1[key] === object2[key]) {
-        return true;
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (eqArrays(object1[key], object2[key])) {
+          continue;
+        } else {
+          return false;
+        }
       } else {
-        return false;
+        // 2) Check the values of each key to see if they match
+        if (object1[key] === object2[key]) {
+        } else {
+          return false;
+        }
       }
     }
+    return true;
   }
   return false;
 };
@@ -34,3 +55,13 @@ assertEqual(eqObjects(ab, ba), true);
 const abc = { a: "1", b: "2", c: "3" };
 eqObjects(ab, abc);
 assertEqual(eqObjects(ab, abc), false);
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+eqObjects(cd, dc);
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+eqObjects(cd, cd2);
+assertEqual(eqObjects(cd, cd2), false);
+
