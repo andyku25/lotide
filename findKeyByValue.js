b3d18f8db6dc122656f1c5a46a96c0c8ff68a700
Andrew <andrew.ku93@gmail.com>
2021-02-03 20:49:04 +0000

    complete the findKeyByValue function using a for/in statement

---
+++ findKeyByValue.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(dataSet, valueSeach) {
+  // for In solution
+  for (const data in dataSet) {
+    if (dataSet[data] === valueSeach) {
+      return data;
+    }
+  }
+
+  // For of solution
+  // for (const data of Object.keys(dataSet)) {
+  //   if (dataSet[data] === valueSeach) {
+  //     return data;
+  //   }
+  // }
+  
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:"The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
