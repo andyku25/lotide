668a3afc38b2a3938c8d97596dfcf53c8607968d
Andrew <andrew.ku93@gmail.com>
2021-02-03 23:22:26 +0000

    refactor to remove extra variable

--- eqArrays.js
+++ eqArrays.js
@@ -8,14 +8,12 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-  let match = true;
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      match = false;
-      return match;
+      return false;
     }
   }
-  return match;
+  return true;
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
