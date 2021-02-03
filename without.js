0803318a3c904aa13fc3edc8f94063554b8b4ea6
Andrew <andrew.ku93@gmail.com>
2021-02-03 01:49:26 +0000

    refactor without function for spread syntax

--- without.js
+++ without.js
@@ -17,11 +17,11 @@ const assertArraysEqual = function(actual, expected) {
 };
 
 const without = function (array1, itemsToRemove) {
-  let results = array1.slice()
+  const results = [...array1]; // Spread syntax instead of .slice()
   for (let i = 0; i < itemsToRemove.length; i++) {
     if (array1.includes(itemsToRemove[i])) {
       // Create a count if to find total number of matches in the array
-      let matchCount = array1.filter(array1Item => array1Item === itemsToRemove[i]).length;
+      const matchCount = array1.filter(array1Item => array1Item === itemsToRemove[i]).length;
       // remove the matched values from the array
       results.splice(array1.indexOf(itemsToRemove[i]), matchCount);
     }
