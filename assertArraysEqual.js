ebc6e01d9fb9f0c6a9e586028c02ad3612c55958
Andrew <andrew.ku93@gmail.com>
2021-02-04 00:02:56 +0000

    update the eqArray function to check if array lengths are the same

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,9 +1,13 @@
 // Check to see if 2 arrays are equal
 const eqArrays = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
     }
+  } else {
+    return false;
   }
   return true;
 };
--- eqArrays.js
+++ eqArrays.js
@@ -8,10 +8,14 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
     }
+  } else {
+    return false;
   }
   return true;
 };
