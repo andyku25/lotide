73d2fcf86bf6b9d3fff69689149ffe4ca22f7bab
Andrew <andrew.ku93@gmail.com>
2021-02-08 19:08:23 +0000

    refactor middle and include a test file

--- middle.js
+++ middle.js
@@ -1,20 +1,5 @@
-// Check to see if 2 arrays are equal
-const eqArrays = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// Import assertArraysEqual
+const assertArraysEqual = require("./assertArraysEqual");
 
 // Find the middle array value(s)
 const middle = function(array) {
@@ -30,18 +15,4 @@ const middle = function(array) {
   }
 };
 
-// Test middle function
-
-assertArraysEqual(middle([1]), [])
-assertArraysEqual(middle([1 ,2]), [])
-assertArraysEqual(middle([1, 2, 3]), [2])
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
-
-// console.log(middle([1]));
-// console.log(middle([1, 2]));
-// console.log(middle([1, 2, 3]));
-// console.log(middle([1, 2, 3, 4]));
-// console.log(middle([1, 2, 3, 4, 5]));
-// console.log(middle([1, 2, 3, 4, 5, 6]));
+module.exports = middle;
